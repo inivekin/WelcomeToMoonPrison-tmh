8f8724a485a931b5f32542bfbaf7c9d2ef05d763
Kevin Cope <inivekin@gmail.com>
2017-05-09 16:18:21 +0800

    Added scene 3 basic start

--- scenes/scene3.js
+++ scenes/scene3.js
@@ -2,28 +2,53 @@ $(document).ready(function () {
     scene3Screen1();
 });
 
-function scene3screen1() {
+function scene3Screen1() {
   showLine('Welcome back to your cell.', 50, 1);
   showLine('You\'ve been out for a while.', 50);
   showLine('You should try to R E L A X .', 50);
 
-  answerOptions(['SIT', 'STAND', 'R E L A X'], ['YES', 'YES', 'OH GOD YES']);
+  answerOptions(['SIT', 'STAND', 'R E L A X'], ['SIT', 'STAND', 'R E L A X']);
 
   $('#ansDiv').on('mousedown.answering', '#ansOp0', function () {
-    console.log('clicked sit');
-  switchOnClick([$('#ansOp1').css('opacity') === '1' || $('#ansOp1').css('opacity') === '0.5',
-                            $('#ansOp1').css('opacity') === '0'],[[0, 1, 1],[0,1,1]], ['You are now sitting. Beautiful.', 'You are now sitting. Beautiful.']);
+  switchOnClick([$('#ansOp0').css('opacity') === '1'], [[1, 1, 1]], ['You are already standing. You can\'t stand and relax, obviously.']);
                           });
   $('#ansDiv').on('mousedown.answering', '#ansOp1', function () {
-    console.log('clicked stand');
-  switchOnClick([$('#ansOp0').css('opacity') === '1' && $('#ansOp1').css('opacity') === '1',
-                            $('#ansOp0').css('opacity') === '0',
-                            $('#ansOp1').css('opacity') === '0.5'], [[1, 0, 1],[1, 0, 1],[ 1, 0, 1]], ['You continue to stand. You are thinking nothing.', 'You are now standing. Horrible.', 'You\'ve been standing a while, perhaps you should SIT and THINK ABOUT WHAT YOU\'VE DONE.']);
+    switchOnClick([$('#ansOp1').css('opacity') === '1'], [[1, 1, 1]], ['Ahh. Almost there.']);
                           });
   $('#ansDiv').on('mousedown.answering', '#ansOp2', function () {
-    console.log('clicked think');
-  switchOnClick([($('#ansOp1').css('opacity') === '1' && $('#ansOp0').css('opacity') === '1') || ($('#ansOp1').css('opacity') === '0.5' && $('#ansOp0').css('opacity') === '1'),
-                            $('#ansOp1').css('opacity') === '1' && $('#ansOp0').css('opacity') === '0',
-                            $('#ansOp1').css('opacity') === '0' && $('#ansOp0').css('opacity') === '1'], [[1, 0.5, 1],[0, 0, 0],[1, 0.5, 1]], ['That would be exhausting.', 'finish', 'That would be exhausting.'], scene1Screen2);
+    switchOnClick([$('#ansOp2').css('opacity') === '1'], [[1, 1, 1]], ['finish'], relaxAudioLoad);
                           });
+}
+
+function relaxAudioLoad() {
+  console.log('Loading audio...');
+  var bufferLoader = new BufferLoader(
+      context,
+      [
+        '../audio/youHaveToRelax.mp3'
+      ],
+      relaxAudioPrepare
+  );
+
+  bufferLoader.load();
+}
+
+function relaxAudioPrepare(bufferList) {
+  console.log('Preparing audio...');
+  var relaxAudio = {};
+
+  relaxAudio['source'] = context.createBufferSource();
+  relaxAudio['source'].buffer = bufferList[0];
+
+  relaxAudio['source'].connect(context.destination);
+
+  scene3Screen2(relaxAudio);
+}
+
+function scene3Screen2 (relaxAudio) {
+  console.log('Beginning Screen2...');
+  $('#ansDiv').off('.answering');
+  clearScreen(0, ['.selectable', '.msg'], 200);
+
+  relaxAudio['source'].start(0);
 }
